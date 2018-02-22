3cb7b2ec74e6076601e354156339f6cd4624ced9
Guillaume RISCHARD <git@stereo.lu>
2018-02-22 23:34:46 +0100

    Test travis build to gh-pages

---
+++ scripts/travis-build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e # Exit with nonzero exit code if anything fails
+
+SOURCE_BRANCH="gh-pages"
+
+# Pull requests and commits to other branches shouldn't try to deploy, just build to verify
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+    echo "Skipping build; just doing a check."
+    # Using xargs to do 8 parallel checks. Not using find -exec because it always returns 0.
+    find sources -name '*.geojson' | xargs -0 -p8 -n4 scripts/convert_xml.py
+else
+    # Run our compile script
+    make
+    # If there are no changes to the compiled out (e.g. this is a README update) then just bail.
+    if diff -q <(git diff -G "^\s*\"generated") <(git diff); then
+        echo "No changes to the imagery files on this push; not deploying."
+    else
+        # Deploy if we reach this far
+        touch .deploy
+    fi
+fi
