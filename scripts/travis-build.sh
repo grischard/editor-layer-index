e6a0ea2aa951ac025557d7afcc5813e942fd8393
Guillaume RISCHARD <git@stereo.lu>
2018-02-23 10:25:27 +0100

    travis build shell script

--- scripts/travis-build.sh
+++ scripts/travis-build.sh
@@ -7,15 +7,8 @@ SOURCE_BRANCH="gh-pages"
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
     echo "Skipping build; just doing a check."
     # Using xargs to do 8 parallel checks. Not using find -exec because it always returns 0.
-    find sources -name '*.geojson' | xargs -0 -P 8 -n 1 scripts/convert_xml.py
+    find sources -name '*.geojson' | xargs -P 8 -n 8 scripts/check.py
 else
     # Run our compile script
     make
-    # If there are no changes to the compiled out (e.g. this is a README update) then just bail.
-    if diff -q <(git diff -G "^\s*\"generated") <(git diff); then
-        echo "No changes to the imagery files on this push; not deploying."
-    else
-        # Deploy if we reach this far
-        touch .deploy
-    fi
 fi
