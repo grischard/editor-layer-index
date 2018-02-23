eebe5bc1d59549b6701588afb504f8a313091e1e
Guillaume RISCHARD <git@stereo.lu>
2018-02-23 12:48:20 +0100

    Move logic from shell scripts to travis

--- scripts/travis-build.sh
+++ scripts/travis-build.sh
@@ -3,11 +3,11 @@ set -e # Exit with nonzero exit code if anything fails
 
 SOURCE_BRANCH="gh-pages"
 
+make check
+
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
     echo "Skipping build; just doing a check."
-    # Using xargs to do 8 parallel checks. Not using find -exec because it always returns 0.
-    find sources -name '*.geojson' | xargs -P 8 -n 8 scripts/check.py
 else
     # Run our compile script
     make
